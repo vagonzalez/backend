c7015d7ddb5d82eb423b6fcb10c751b19a72c2ee
Jonatan Rodriguez <jrperdomoz@gmail.com>
2017-02-16 23:59:38 +0200

    clean loader

--- src/helpers/loader.js
+++ src/helpers/loader.js
@@ -9,9 +9,26 @@ import fs from 'fs'
 import path from 'path'
 import Promise from 'bluebird'
 import { ERROR } from './errors'
-import { reduceExports, reduceModule } from '../reducers/'
 
 const filterFiles = (f) => !['index.js'].includes(f)
+
+const reduceExports = (res, row) => {
+  res[row.filename] = row.fun
+  return res
+}
+
+// const reduceModules = (res, row) => {
+//   res['filename'] = row.filename
+//   res['fun'] = row.fun
+//   return res
+// }
+
+const reduceModule = (res, row) => {
+  Object.keys(row.fun).forEach((key) => {
+    res[key] = row.fun[key]
+  })
+  return res
+}
 
 const getExport = (dir, file) => new Promise((resolve, reject) => {
   let filename = path.basename(file, '.js')
