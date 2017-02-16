fc99163ac0db736ef9db21072ca95dc980d39c81
Vidal González <vidal.gonzalez@gmail.com>
2017-02-16 23:58:00 +0000

    Se introducen los settings de base de datos para inicializar la conexión a la base de datos

--- src/helpers/connection.js
+++ src/helpers/connection.js
@@ -9,15 +9,24 @@ const DISCONNECTED = 'Mongoose default connection disconnected'
 const ERROR = 'Mongoose default connection errors: '
 const OK = 'Mongoose connection open to mongodb://localhost/'
 
-const mongooseConnection = (dbName) => new Promise((resolve, reject) => {
-  mongoose.connect('mongodb://localhost/' + dbName, (err) => {
+const getConnectionString = (settings) => {
+  const auth = settings.user ? `${settings.user}:${settings.password}@` : ''
+  const port = settings.port ? `:{settings.port}/` : '/'
+
+  return `${auth}${settings.server}${port}${settings.db}`
+}
+
+const mongooseConnection = (dbSettings) => new Promise((resolve, reject) => {
+  const connectionString = getConnectionString(dbSettings)
+
+  mongoose.connect(`mongodb://${connectionString}`, (err) => {
     if (err) return reject(err)
     initialize('models')
     mongoose.middleware = mongooseMiddleware
     mongoose.Promise = Promise
     mongoose.connection.on('error', (err) => console.log(chalk.red(ERROR + err)))
     mongoose.connection.on('disconnedcted', () => console.log(chalk.red(DISCONNECTED)))
-    console.log(chalk.green(OK + dbName))
+    console.log(chalk.green(OK + dbSettings.db))
     return resolve(mongoose)
   })
 })
