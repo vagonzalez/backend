3501893bd4df0ce0fbf6e10510f154e8ae305db5
Vidal González <vidal.gonzalez@gmail.com>
2017-02-16 13:58:14 +0000

    Se implementan los cambios necesarios para que funcionen las rutas de joni y de vidal

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import express from 'express'
 import mongoose from 'mongoose'  // eslint-disable-line
 import morgan from 'morgan'
 
+import authentication from './middlewares/AuthenticationMiddleware'
 import mongooseConnection from './helpers/connection'
-import router from './router'
+import { vRouter, router } from './router'
 
 const app = express()
 mongooseConnection('dev')
@@ -14,9 +15,11 @@ mongooseConnection('dev')
     .use(cors())
     .use(urlencoded({ extended: false }))
     .use(json())
+    .use(authentication(true))
     .use(mongoose.middleware)
     .use(morgan('dev'))
     .use('/', router)
+    .use('/vidal', vRouter)
     .listen(3000, () => console.log('http://localhost:3000/'))
   })
   .catch((err) => console.log(err))
