39b20e3653d3ada649dbc847a4171ee576ad11c6
Vidal González <vidal.gonzalez@gmail.com>
2017-02-16 13:55:45 +0000

    Se añade lo que podría ser el core de la aplicación, desde donde se carga la configuración, middlewares, etc

---
+++ src/core/Server.js
@@ -0,0 +1,8 @@
+// Aquí debería ir el arranque del server, cargando su configuración
+// Quizá inyección de dependencias
+
+
+// Obtiene la configuración de la aplicación
+const getProjectSettings = () = {
+  return {}
+}
